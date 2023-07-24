79de7f8e125f838c0e373a2ffff622d973b0ff3a
MartinKotouc <mkotouc@email.cz>
2023-07-24 16:26:20 +0200

    Initial commit

---
+++ src/main/java/com/example/MKOshop/MkOshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.MKOshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkOshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkOshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MKOshop/MkOshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MKOshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkOshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
